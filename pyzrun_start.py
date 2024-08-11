2b73372e44028146639b69de37b06416ec7fc078
韩强飞 <hanqiangfei@163.com>
2024-08-11 14:51:40 +0800

    INIT

---
+++ pyzrun_start.py
@@ -0,0 +1,36 @@
+import pgzrun
+
+WIDTH = 800
+HEIGHT = 600
+MAX_BULLETS=10
+bullets=[]
+
+player=Actor("actor")
+player.pos=(300,500)
+
+def draw():
+    screen.clear()
+    screen.draw.circle((400, 300), 30, 'white')
+    player.draw()
+    for bullet in bullets:
+        bullet.draw()
+
+def update():
+    move_bullets()
+    if keyboard.right:
+        player.x=alien.x+1
+
+def move_bullets():
+    #pass
+    for bullet in bullets:
+        bullet.y = bullet.y - 6
+        if bullet.y < 0:
+            bullets.remove(bullet)
+
+def on_key_down(key):
+    if key==keys.SPACE and len(bullets) <MAX_BULLETS:        
+        print("bullet")
+        bullet=Actor("bullet",pos=(player.x,player.y))
+        bullets.append(bullet)
+
+pgzrun.go()
